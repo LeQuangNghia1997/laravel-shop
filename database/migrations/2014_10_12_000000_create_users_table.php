e9a0eb9a8470698ee24ee69edf108eee8432feb8
jinsiko <45958331+jinsiko@users.noreply.github.com>
2019-04-10 17:54:15 +0700

    Revert "Setupdatauser"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address');
-            $table->string('phone');
-            $table->string('image')->nullable();
-            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
